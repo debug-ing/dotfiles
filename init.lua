fc686b0c6ad75563b852027e79b8cf1ee5a15b48
Mahdi Mohammadi <46979964+debug-ing@users.noreply.github.com>
2025-07-08 12:55:52 +0330

    feat: add init.lua for nvim

---
+++ init.lua
@@ -0,0 +1,70 @@
+-- Bootstrap lazy.nvim if not installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+require("lazy").setup({
+  -- LSP Config
+  { "neovim/nvim-lspconfig" },
+
+  -- Autocomplete Engine
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+    },
+  },
+
+  -- File Explorer
+  { "nvim-tree/nvim-tree.lua" },
+  -- codium
+  "Exafunction/codeium.vim",
+})
+
+-- Basic LSP setup (example: gopls for Go)
+local lspconfig = require("lspconfig")
+lspconfig.gopls.setup {}
+
+-- Basic nvim-cmp setup
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+  }),
+})
+
+-- nvim-tree setup
+require("nvim-tree").setup()
+
+-- Basic keymap for nvim-tree toggle
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- Some nice options
+vim.o.number = true
+vim.o.relativenumber = true
