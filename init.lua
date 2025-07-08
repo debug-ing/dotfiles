0d49c7c6b01e0a022fa020fc8f647a5cb0bdd248
Mahdi Mohammadi <46979964+debug-ing@users.noreply.github.com>
2025-07-08 13:45:47 +0330

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- pre install
 -- go install golang.org/x/tools/gopls@latest
 -- export PATH=$PATH:$HOME/go/bin
+-- https://github.com/akinsho/toggleterm.nvim
 -- Bootstrap lazy.nvim if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
